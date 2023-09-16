719b697f6eaa13af9c77106d9b7349263549f9f5
Rekaya Nour <nourrkaya@live.be>
2023-09-16 17:07:08 +0200

    0.2 : app express creation & HTML reminder

---
+++ exercices/basic-generated-app/public/index.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body");
+
+body.addEventListener("click", startOrStopSound);
+
+function startOrStopSound() {
+  const myAudioPlayer = document.querySelector("#audioPlayer");
+
+  if (myAudioPlayer.paused) myAudioPlayer.play();
+  else myAudioPlayer.pause();
+}
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
