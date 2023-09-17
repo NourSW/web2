a34286041494bad6283b8f1fbfee144ccd159b98
Rekaya Nour <nourrkaya@live.be>
2023-09-17 17:44:24 +0200

    1.2 : app middleware

---
+++ tutorials/pizzeria/api/basic/app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var pizzaRouter = require('./routes/pizzas');
+var usersRouter = require('./routes/users');
+
+var app = express();
+let nmbrReq = 0;
+
+app.use((req, res, next) => {
+    console.log('Time:', Date.now());
+    console.log('nmbr req : ' ,nmbrReq);
+    next();
+    nmbrReq++;
+  });
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/pizzas', pizzaRouter);
+app.use('/users', usersRouter);
+
+
+
+module.exports = app;
